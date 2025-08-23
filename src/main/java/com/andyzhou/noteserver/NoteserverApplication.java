b40a5cd18fed6378a073c01ae9d441a69b4b9f1d
Andy Zhou <andyzhou417@gmail.com>
2025-08-23 17:51:36 -0400

    first commit

---
+++ src/main/java/com/andyzhou/noteserver/NoteserverApplication.java
@@ -0,0 +1,13 @@
+package com.andyzhou.noteserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andyzhou/noteserver/NoteserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andyzhou.noteserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
